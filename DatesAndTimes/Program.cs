e132980ce7506d0a218e7bcdc6fcb28c7ec4dae0
Redninja1 <james@redninja.net>
2016-10-08 19:38:23 +0100

    simple date and times in CS

---
+++ DatesAndTimes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            Console.WriteLine(myValue.ToString());
+
+            Console.WriteLine(myValue.ToShortTimeString());
+
+           Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+
+            DateTime myBirthday = DateTime.Parse("20/12/1987");
+            TimeSpan muAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(muAge.TotalDays);
+
+            Console.ReadLine();
+        }
+    }
+}
